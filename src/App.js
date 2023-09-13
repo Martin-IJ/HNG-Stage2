854148e6831f0128f6ed870dd1811a99e3762df1
martins <martinohez@gmail.com>
2023-09-13 05:29:38 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='bg-blue-500 text-white'>Martins MovieBox</h1>
+    </div>
+  );
+}
+
+export default App;
