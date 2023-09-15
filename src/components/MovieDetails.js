cb053749e1a8e266beed24404e572c2e8ef3a61f
martins <martinohez@gmail.com>
2023-09-16 00:23:12 +0100

    slight correction

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -24,7 +24,7 @@ const MovieDetails = () => {
         const data = await response.json();
         setMovieDetails(data);
       } catch (error) {
-        console.error(error);
+        console.log(error);
       }
     };
 
