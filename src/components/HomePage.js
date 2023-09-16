33a75117f9c0f2590352b508873b5d35c2d78779
martins <martinohez@gmail.com>
2023-09-16 06:00:39 +0100

    fixed some loading errors

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -7,20 +7,28 @@ import PlayBtn from "../assets/Play.svg";
 import SearchBar from "./SearchBar";
 import MovieCard from "./MovieCard";
 import Footer from "./Footer";
-import loadingImg from '../assets/loading.gif'
+import loadingImg from "../assets/loading.gif";
 import SearchContent from "./SearchContent";
 import { useGlobalContext } from "../Context";
 
 const HomePage = () => {
-    const {content, movies} = useGlobalContext()
+  const { content, movies, loading } = useGlobalContext();
 
-    if (!movies && !content) {
-        return (
-          <div className="m-auto text-center flex items-center h-screen">
-            <img src={loadingImg} alt="loading" className="m-auto" />
-          </div>
-        );
-      }
+  if (loading) {
+    return (
+      <section className="m-auto flex items-center text-center">
+        <img src={loadingImg} alt="loading" className="m-auto" />
+      </section>
+    );
+  }
+
+  if (!movies && !content) {
+    return (
+      <div className="m-auto text-center flex items-center h-[100vh]">
+        <p>No data available.</p>
+      </div>
+    );
+  }
 
   return (
     <main className="text-white font-sans">
@@ -48,7 +56,8 @@ const HomePage = () => {
             <div className="flex">
               <img src={FilmLogo} alt="movie logo" />{" "}
               <span className="ml-3 mr-7">86.0 / 100</span>{" "}
-              <img src={Fruit} alt="Rated movies" /> <span className="ml-3">97%</span>
+              <img src={Fruit} alt="Rated movies" />{" "}
+              <span className="ml-3">97%</span>
             </div>
             <p className="text-[14px] font-[500] leading-[18px] max-w-[305px]">
               John Wick is on the run after killing a member of the
